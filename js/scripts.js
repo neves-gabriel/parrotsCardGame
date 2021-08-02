14d8c0423d4001d854f2492ccac64d09f294dc5d
Gabriel Neves <gabrielrn.mobres@gmal.com>
2021-08-02 11:48:55 -0300

    embaralhar cartas adicionado

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
-let QtdeCartas = 0;
+let qtdeCartas = 0;
 let segundos = 0;
 let intervalo;
 const relogio = document.getElementById("relogio");
+const tiposCartas = ["bobross", "explody", "fiesta", "metal", "revertit", "triplets", "unicorn"]
 
 
 
@@ -10,8 +11,8 @@ const relogio = document.getElementById("relogio");
 perguntarQtde();
 
 function perguntarQtde() {
-    while (QtdeCartas > 14 || QtdeCartas < 4 || QtdeCartas % 2 !== 0) {
-        QtdeCartas = parseInt(prompt("Com quantas cartas você quer jogar? Escolha números pares de 4 a 14"));
+    while (qtdeCartas > 14 || qtdeCartas < 4 || qtdeCartas % 2 !== 0) {
+        qtdeCartas = parseInt(prompt("Com quantas cartas você quer jogar? Escolha números pares de 4 a 14"));
     }
     montarBaralho();
     iniciarRelogio();
@@ -29,5 +30,34 @@ function iniciarRelogio () {
 function contarSegundos() {
     segundos++;
     relogio.innerHTML = segundos;
+}
+
+function pararRelogio() {
+    contarSegundos();
+    clearInterval(intervalo);
+}
+
+function montarBaralho() {
+    embaralharCartas();
+    document.getElementById("baralho").innerHTML = "";
+    for (let i = 0; i < qtdeCartas; i++) {
+        document.getElementById("baralho").innerHTML += 
+        `<li class="carta ${cartasSelecionadas[i]}">
+            <div class="frente face">
+            </div>
+            <div class="costa face">
+            </div>
+        </li>`;
+    }
+}
+
+function embaralharCartas() {
+    cartasSelecionadas = tiposCartas.sort(comparador).slice(0, qtdeCartas / 2);
+    cartasSelecionadas = cartasSelecionadas.concat(cartasSelecionadas);
+    cartasSelecionadas = cartasSelecionadas.sort(comparador);
+}
+
+function comparador() {
+    return Math.random() - 0.5;
 }
 
